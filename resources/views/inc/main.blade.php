ca21ed26be1ca577e0c4f05d2471c9884c8c6831
Giancarlos Garza <giancarlosgza@gmail.com>
2019-01-17 14:54:39 -0600

    Search Query input
    Search input added

--- resources/views/inc/main.blade.php
+++ resources/views/inc/main.blade.php
@@ -1,15 +1,32 @@
 <div class="container">
     <div class="row">
         <div class="col-md-6">
-            <h1 class="logo">
+            <h1 class="logo text-body">
                 Color Gradients and Palettes!
             </h1>
             <h4 class="intro-text">
-                Look, choose and use for free any colors from this collection. 
+                Look, choose and use for free any colors from this collection.
             </h4>
         </div>
-        <div class="col-md-6">
-
+    </div><br>
+    <div class="row">
+        <div class="col-6 col-sm-6">
+            <a href="/gradients/catalog">
+                <div class="card card-white shadow-medium">
+                    <div class="card-body ">
+                        <h6 class="logo"><i class="fas fa-circle gradient"></i> GRADIENTS</h6>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-6 col-sm-6">
+            <a href="/palettes/catalog">
+                <div class="card card-white shadow-medium">
+                    <div class="card-body">
+                        <h6 class="logo"><i class="fas fa-circle palette"></i> PALETTES</h6>
+                    </div>
+                </div>
+            </a>
         </div>
     </div>
-</div><br>
+</div>
--- resources/views/pages/palette/catalog/catalog.blade.php
+++ resources/views/pages/palette/catalog/catalog.blade.php
@@ -7,6 +7,17 @@
         <div class="col-sm-12">
             <h4 class="bold">Palettes</h4>
         </div>
+        <div class="col-sm-12">
+            <form action="" method="get">
+                <div class="input-group mb-3">
+                    <input type="text" name="q" id="query" value="{{ $searchQuery }}" class="form-control shadow-medium no-border"
+                        placeholder="Search Palettes" aria-label="Search Palettes" aria-describedby="button-addon2">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="submit" id="button-addon2">Search</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
     <br>
     <div class="row">
